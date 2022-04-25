ad5e756edc7d06c4cdb5e861bcc088d23f52f949
muthanna872bno <95106207+muthanna872bno@users.noreply.github.com>
2022-04-25 15:48:19 +0300

    Add files via upload

---
+++ Template.js
@@ -0,0 +1,6 @@
+document.write("<h1>Hello<span> Page </span></h1> ");
+// document.createElement("")
+console.log("Hello From %cJs %cFile","color:red; font-size: 40px","color:blue; font-size: 40px");//بحط رساله فالكونسل
+
+console.error("Error");
+console.table(["Muthanna", "Mohammad","Ameen"]);
